3f2774e35934e3677e64e79a5168f209a51d068e
Jacques Visser <jacquesvisserjnr@gmail.com>
2020-11-16 14:55:59 -0800

    Made the ball move, Added Vsync and deltaTime

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Audio.hpp>
@@ -7,49 +8,34 @@
 #include <SFML/OpenGL.hpp>
 #include <SFML/Main.hpp>
 
+using namespace std;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1024, 1024), "GAME 230 - Pong");
+    //Define Window dimensions and program title
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "GAME 230 - Pong");
+    window.setVerticalSyncEnabled(true);
 
-    sf::Font font;
-    if (!font.loadFromFile("data/Fonts/OpenSans-Regular.ttf")) {
-        return 1;
-    }
-
-    sf::Text title;
-    title.setFont(font);
-    title.setString("Pong!");
-    title.setCharacterSize(42);
-    title.setFillColor(sf::Color::Red);
-    //title.setStyle(sf::Text::Regular);
-    title.setPosition(400, 20);
-
-    sf::SoundBuffer buffer;
-    if (!buffer.loadFromFile("data/Sounds/sound.ogg")) {
-        return 1;
-   }
-
-    sf::Sound sound;
-    sound.setBuffer(buffer);
-    sound.play();
-
-    sf::Texture iconTexture;
-    if (!iconTexture.loadFromFile("data/Textures/UnityIcon.png"))
-    {
-        return 1;
-    }
-
-    iconTexture.setSmooth(true);
-    sf::Sprite icon;
-    icon.setTexture(iconTexture);
-    icon.setScale(0.5f, 0.5f);
-
+    //Define shape as circle
     sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    //Set the circle Position
+    shape.setPosition(412, 284);
+    //Define Purple
+    sf::Color Purple(90, 24, 154);
+    //Set the colour of the circle
+    shape.setFillColor(Purple);
 
+    sf::Clock clock;
+    float deltaTime = 0.0f;
+
+
+    //Update
     while (window.isOpen())
     {
+        deltaTime = clock.getElapsedTime().asSeconds();
+        clock.restart();
+        cout << deltaTime;
+
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -57,10 +43,12 @@ int main()
                 window.close();
         }
 
+        auto position = shape.getPosition();
+        shape.setPosition(position.x + 0.1, position.y + 0.1);
+
+        //Render
         window.clear();
         window.draw(shape);
-        //window.draw(title);
-        //window.draw(icon);
         window.display();
     }
 
