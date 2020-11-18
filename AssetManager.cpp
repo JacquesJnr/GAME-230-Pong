e0c16c30baf51be4efcb40b1d5685814f42b7fa5
Jacques Visser <jacquesvisserjnr@gmail.com>
2020-11-18 12:56:46 -0800

    Added asset manager for fonts, textures and sounds

---
+++ AssetManager.cpp
@@ -0,0 +1,42 @@
+#include "AssetManager.h"
+
+namespace Pong 
+{
+	void AssetManager::LoadTexture(std::string name, std::string path)
+	{
+		sf::Texture tex;
+
+		if (tex.loadFromFile(path)) {
+			this->_textures[name] = tex;
+		}
+	}
+
+	sf::Texture &AssetManager::GetTexture(string name)
+	{
+		return this->_textures.at(name);
+	}
+
+	void AssetManager::loadFont(std::string name, std::string path)
+	{
+		sf::Font font;
+
+		if (font.loadFromFile(path)) {
+			this->_fonts[name] = font;
+		}
+	}
+
+	sf::Font& AssetManager::GetFont(string name)
+	{
+		return this->_fonts.at(name);
+	}
+
+	void AssetManager::LoadSound(string name, string path)
+	{
+	}
+
+	sf::SoundBuffer& AssetManager::GetBuffer(string name)
+	{
+		// TODO: insert return statement here
+	}
+
+}
--- AssetManager.h
+++ AssetManager.h
@@ -1,29 +1,34 @@
 #pragma once
 #include <map>
 #include<SFML/Graphics.hpp>
+#include<SFML/Audio.hpp>
 using namespace std;
 
-namespace Pong 
+namespace Pong
 {
+
 	class AssetManager
 	{
 	public:
 
-		AssetManager() {}
+		AssetManager();
 		~AssetManager() {}
 
-		void LoadTexture(std::string name, std::string fileName) {
-			sf::Texture& GetTexture(std::string name);
-		}
+		void LoadTexture(string name, string path);
+		sf::Texture &GetTexture( string name );
 
-		void loadFont(std::string name, std::string fileName) {
-			sf::Font& GetFont(std::string name);
-		}
+		void loadFont(string name, string path);
+		sf::Font &GetFont( string name );
+
+		void LoadSound(string name, string path);
+		sf::SoundBuffer& GetBuffer(string name);
 
 	private:
 		map<string, sf::Texture>  _textures;
 		map<string, sf::Font>  _fonts;
+		map<string, sf::SoundBuffer>_sounds;
 	};
+
 }
 
 
