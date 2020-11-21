539fc285f48367f37042a0f4aef9408c75fba84e
Jacques Visser <jacquesvisserjnr@gmail.com>
2020-11-20 17:08:32 -0800

    Update

---
+++ GameMaster.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <SFML/Graphics.hpp>
+#include "StateMachine.h"
+#include "AssetManager.h"
+#include "InputManager.h"
+using namespace std;
+
+namespace Pong 
+{
+	struct Game
+	{
+		StateMachine theMachine;
+		sf::Window window;
+		AssetManager asstes;
+		InputManager input;
+	};
+
+	typedef shared_ptr<Game> GameDataRef;
+
+	class GameMaster {
+	public:
+		GameMaster(int width, int height, string title);
+
+	private:
+		const float deltaTime = 1.0 / 60.0f;
+		sf::Clock _clock;
+		GameDataRef  _gameData = make_shared<Game>();
+
+		void Run( );
+	};
+}
+
