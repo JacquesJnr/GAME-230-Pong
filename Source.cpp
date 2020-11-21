266a1523b6ce1f2f6b192507f090f0d3deef3180
Jacques Visser <jacquesvisserjnr@gmail.com>
2020-11-20 17:06:43 -0800

    Added Definitions.h

--- Source.cpp
+++ Source.cpp
@@ -1,25 +1,22 @@
 #include <cmath>
 #include <iostream>
-#include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
-#include <SFML/Audio.hpp>
-#include <SFML/Network.hpp>
 #include <SFML/System.hpp>
-#include <SFML/OpenGL.hpp>
 #include <SFML/Main.hpp>
 #include "Collider.h"
 #include "Paddle.h"
 #include "Ball.h"
-
+#include "GameMaster.h"
 using namespace std;
+using namespace Pong;
 
-const int WIDTH = 1024;
-const int HEIGHT = 768;
-const int _ballSpeed = 500.0f;
-const int _paddleSpeed = 800.0f;
+#define WIDTH 1024;
+#define HEIGHT 768;
 
 int main()
 {
+	Pong::Game(1024, 768, "SFML");
+
 	return EXIT_SUCCESS;
 }
 
